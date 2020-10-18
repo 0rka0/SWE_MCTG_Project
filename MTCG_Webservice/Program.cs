8d0b63d302d4cf211483a197c5bee0a83043c98c
David Friedwagner <david.fw3004@gmail.com>
2020-10-18 12:56:40 +0200

    Webservice Project added

---
+++ MTCG_Webservice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MTCG_Webservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
