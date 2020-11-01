e32a7117532ae644b98cfaa2ca6d07501d01d198
David Friedwagner <david.fw3004@gmail.com>
2020-11-01 16:04:18 +0100

    server client sample implementation

---
+++ MTCG_Project_Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MTCG_Project_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(2000); // 2 sec so the server is up and running
+
+            using TcpClient client = new TcpClient("localhost", 8000);
+            using StreamReader reader = new StreamReader(client.GetStream());
+            Console.WriteLine(reader.ReadLine());
+            using StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+
+            string input = null;
+            Console.WriteLine("Enter command:");
+            while ((input = Console.ReadLine()) != "X")
+            {
+                writer.WriteLine(input);
+                Console.WriteLine("Enter command:");
+            }
+            writer.WriteLine("X");
+
+        }
+    }
+}
---
+++ MTCG_Project_Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace MTCG_Project_Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 8000);
+            listener.Start(5);
+
+            Console.CancelKeyPress += (sender, e) => Environment.Exit(0);
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Waiting for connection...");
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+                    using StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+                    Console.WriteLine("Connected!");
+                    writer.WriteLine("Welcome to the MTCG Server!");
+
+                    using StreamReader reader = new StreamReader(client.GetStream());
+                    string message;
+                    do
+                    {
+                        message = reader.ReadLine();
+                        Console.WriteLine("received: " + message);
+                    } while (message != "X");
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("error occurred: " + exc.Message);
+                }
+            }
+        }
+    }
+}
