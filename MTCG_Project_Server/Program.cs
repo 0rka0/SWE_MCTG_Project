887dc3c78fadea66267513229a4cd58ceba3d90d
David Friedwagner <david.fw3004@gmail.com>
2020-11-03 09:52:01 +0100

    Simple server structure

--- MTCG_Project_Server/Program.cs
+++ MTCG_Project_Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -8,8 +9,11 @@ namespace MTCG_Project_Server
 {
     class Program
     {
+        static List<string> MessageList = new List<string>();
+        static string[] inputMsg = new string[8];
         static async Task Main(string[] args)
         {
+            //List<string> tmpInputMsg = new List<string>();
             TcpListener listener = new TcpListener(IPAddress.Loopback, 8000);
             listener.Start(5);
 
@@ -21,23 +25,83 @@ namespace MTCG_Project_Server
                 {
                     Console.WriteLine("Waiting for connection...");
                     TcpClient client = await listener.AcceptTcpClientAsync();
-                    using StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
                     Console.WriteLine("Connected!");
-                    writer.WriteLine("Welcome to the MTCG Server!");
 
                     using StreamReader reader = new StreamReader(client.GetStream());
-                    string message;
-                    do
+                    using StreamWriter writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+                    
+                    ReadHeader(reader);
+
+                    if (String.Compare(inputMsg[0], "DELETE") != 0)
                     {
-                        message = reader.ReadLine();
-                        Console.WriteLine("received: " + message);
-                    } while (message != "X");
+                        reader.ReadLine();
+                        ReadContent(reader);
+                    }
+                    
+                    for (int i = 0; i < 8; i++)
+                        Console.WriteLine(inputMsg[i]);
                 }
                 catch (Exception exc)
                 {
                     Console.WriteLine("error occurred: " + exc.Message);
                 }
             }
+        }
+
+        static void ReadHeader(StreamReader reader)
+        {
+            ReadFirstLine(reader);
+
+            for (int i = 3; i < 6; i++)
+            {
+                ReadCurLine(reader, i);
+            }
+
+            if (String.Compare(inputMsg[0], "DELETE") != 0)
+            {
+                ReadCurLine(reader, 6);
+            }
+            else
+            {
+                inputMsg[6] = "";
+                inputMsg[7] = "";
+            }
+        }
+
+        static void ReadFirstLine(StreamReader reader)
+        {
+            string tmpString;
+            int counter = 0;
+            tmpString = reader.ReadLine();
+            Console.WriteLine("received: " + tmpString);
+
+            string[] tmpStrList = tmpString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string s in tmpStrList)
+            {
+                inputMsg[counter] = s;
+                counter++;
+            }
+        }
+
+        static void ReadCurLine(StreamReader reader, int counter)
+        {
+            string tmpString = "";
+            tmpString = reader.ReadLine();
+            int index = tmpString.IndexOf(": ");
+            inputMsg[counter] = tmpString.Substring(index + 2);
+            Console.WriteLine("received: " + tmpString);
+        }
+
+        static void ReadContent(StreamReader reader)
+        {
+            string tmpString = "";
+            while(!reader.EndOfStream)
+            {
+                tmpString += reader.ReadLine();
+            }
+            inputMsg[7] = tmpString;
+            Console.WriteLine("received: " + tmpString);
         }
     }
 }
---
+++ MTCG_Project_Server/message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTCG_Project_Server
+{
+    public struct Message
+    {
+        public string HttpVerb { get; private set; }
+        public string Endpoint { get; private set; }
+        public string Protocol { get; private set; }
+        public string Host { get; private set; }
+        public string User_Agent { get; private set; }
+        public string Accept { get; private set; }
+        public string Content_Length { get; private set; }
+        public string Content { get; private set; }
+
+        public Message(string p1, string p2, string p3, string p4, string p5, string p6, string p7, string p8)
+        {
+            HttpVerb = p1;
+            Endpoint = p2;
+            Protocol = p3;
+            Host = p4;
+            User_Agent = p5;
+            Accept = p6;
+            Content_Length = p7;
+            Content = p8;
+        }
+    }
+}
